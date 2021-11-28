77c8e8126d32b2d6d71f6b9ca106a4fe27b97074
Ramazan Ceylan <Ramazan.CEYLAN@halkbank.com.tr>
2021-11-28 16:35:43 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Ramazan Ceylan on 28.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
